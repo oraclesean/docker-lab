51d8fe2562704d202847cbe853997914f8eb011a
oraclesean <oracle.sean@gmail.com>
2021-09-17 15:47:09 -0600

    Fix attribute

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  availability_domain        = data.oci_identity_availability_domains.ad_list.availability_domains.0.nameic_key
+  availability_domain        = data.oci_identity_availability_domains.ad_list.availability_domains.0.name
   private_key                = try(file(var.private_key_path), var.private_key)
 }
